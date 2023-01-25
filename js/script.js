f2e1b4480c2b8d44edc1275c626ec6551d7dc8ac
`thais` <`tjlannes@gmail.com`>
2023-01-25 17:28:58 +0100

    changes of the exercise

--- js/script.js
+++ js/script.js
@@ -1,31 +1,13 @@
-function areBothValuesNaN(valueA, valueB) {
-  //isStrictNaN, confirma se os dois sao NaN atraves do Number.isNaN, vai sair true - na funçao principal, coloco ! para que sea falso
-  return Number.isNaN(valueA) && Number.isNaN(valueB);
+function isPalindrome(sentence) {
+  if (sentence === " ") {
+    console.log("Text not provided");
+  } else {
+    let minuscula = sentence
+      .toLowerCase()
+      .replaceAll(" ", "")
+      .replace(/[^a-z]/gi, "");
+    return minuscula;
+  }
 }
 
-function areIntegers(valueA, valueB) {
-  return Number.isInteger(valueA) && Number.isInteger(valueB);
-}
-
-function isStrictZero(valueA, valueB) {
-  //Zero, confirma se sao os dois numeros inteiros, se sim, passa adiante, se sim, cambia os true dos valores a false.
-  return areIntegers(valueA, valueB) && !valueA && !valueB;
-}
-
-function strictEquals(valueA, valueB) {
-  return (
-    (Object.is(valueA, valueB) && !areBothValuesNaN(valueA, valueB)) ||
-    isStrictZero(valueA, valueB)
-  );
-}
-
-console.log(strictEquals(1, 1)); // true
-console.log(strictEquals(NaN, NaN)); // false
-console.log(strictEquals(0, -0)); // true
-console.log(strictEquals(-0, 0)); // true
-console.log(strictEquals(1, "1")); // false
-console.log(strictEquals(true, false)); // false
-console.log(strictEquals(false, false)); // true
-console.log(strictEquals("Water", "oil")); // false
-console.log(strictEquals("Water", "Water")); // true
-console.log(strictEquals("", 0)); // false
+isPalindrome("Mano Brown*_3");
